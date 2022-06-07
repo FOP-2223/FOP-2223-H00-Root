19073e19b203c32089b76cc4ae880e582714b3d2
Rdeisenroth <53945743+Rdeisenroth@users.noreply.github.com>
2022-06-08 01:41:40 +0200

    Add Grader (#3)
    * Add Tests and Rubric Provider
    
    * Fix Checkstyle errors, add readme and more cleanup

--- src/grader/java/h00/H00_RubricProvider.java
+++ src/grader/java/h00/H00_RubricProvider.java
@@ -1,5 +1,8 @@
 package h00;
 
+import org.sourcegrade.jagr.api.rubric.Criterion;
+import org.sourcegrade.jagr.api.rubric.Grader;
+import org.sourcegrade.jagr.api.rubric.JUnitTestRef;
 import org.sourcegrade.jagr.api.rubric.Rubric;
 import org.sourcegrade.jagr.api.rubric.RubricForSubmission;
 import org.sourcegrade.jagr.api.rubric.RubricProvider;
@@ -7,12 +10,85 @@ import org.sourcegrade.jagr.api.rubric.RubricProvider;
 @RubricForSubmission("h00")
 public class H00_RubricProvider implements RubricProvider {
 
-    public static final Rubric RUBRIC = Rubric.builder()
-        .title("H00")
-        .build();
-
     @Override
     public Rubric getRubric() {
-        return RUBRIC;
+        return Rubric
+            .builder()
+            .title("H00")
+            .addChildCriteria(
+                Criterion
+                    .builder()
+                    .shortDescription("H4: Erste Schritte mit FOPBot")
+                    .addChildCriteria(
+                        Criterion
+                            .builder()
+                            .shortDescription("Der Roboter erreicht die obere rechte Ecke mindestens einmal")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testUpperRightCornerReached")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die Endposition des Roboters ist immer exakt korrekt.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die Bewegungen des Roboters "
+                                + "(Position und Blickrichtung) "
+                                + "sind vollständig korrekt.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die korrekte Anzahl an Münzen wurde an den geforderten Stellen richtig platziert.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build()
+                    )
+                    .build()
+            )
+            .build();
     }
 }
--- src/main/java/h00/Main.java
+++ src/main/java/h00/Main.java
@@ -1,39 +1,43 @@
 package h00;
 
-import fopbot.Direction;
 import fopbot.Robot;
 import fopbot.World;
 
-import java.lang.reflect.Executable;
-import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static fopbot.Direction.*;
+import static fopbot.Direction.DOWN;
+import static fopbot.Direction.UP;
 
 /**
- * Code für Übungsblatt 00
+ * Code für Übungsblatt 00.
  *
  * @author Ruben Deisenroth
  */
 public class Main {
 
-    public static void main(String[] args) {
+    /**
+     * Starts the Program.
+     *
+     * @param args the Launch arguments
+     */
+    public static void main(String... args) {
+        // Welt mit Größe 5x5 erstellen
         final int worldSize = 5;
         World.setSize(worldSize, worldSize);
-        World.setDelay(50);
+
+        // Die Schrittdauer auf 200ms setzen
+        World.setDelay(200);
+
+        // Die Welt anzeigen
         World.setVisible(true);
-        Robot robby = new Robot(4, 0, DOWN, 12);
-        doExercise(robby);
+
+        // Den Code der Hausübung ausführen
+        doExercise();
     }
 
     /**
-     * Bewegt den Roboter entsprechend den Anforderungen von Übungsblatt 00
-     *
-     * @param robby der Roboter, der bewegt werden soll
+     * Bewegt den Roboter entsprechend den Anforderungen von Übungsblatt 00.
      */
-    public static void doExercise(Robot robby) {
+    public static void doExercise() {
+        Robot robby = new Robot(4, 0, DOWN, 12);
         // TODO H00 Implement your solution here:
 
         // Zunächst drehen wir uns nach oben
@@ -48,7 +52,7 @@ public class Main {
         // nach links schauen
         robby.turnLeft();
 
-        // jetzt gehen wir stufenweise nach Unten links
+        // jetzt gehen wir stufenweise nach unten links
         for (int i = 0; i < 4; i++) {
             // Schritt nach Links + Münze legen
             robby.move();
