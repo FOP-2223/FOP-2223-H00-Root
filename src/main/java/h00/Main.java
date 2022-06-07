bbb107dedb0d6365c30c5e0d5232f0d2f5bacb67
Rdeisenroth <53945743+Rdeisenroth@users.noreply.github.com>
2022-06-07 20:54:46 +0200

    Write Solution (#1)

--- src/main/java/h00/Main.java
+++ src/main/java/h00/Main.java
@@ -1,16 +1,70 @@
 package h00;
 
+import fopbot.Direction;
+import fopbot.Robot;
+import fopbot.World;
+
+import java.lang.reflect.Executable;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static fopbot.Direction.*;
+
 /**
- * Main entry point in executing the program.
+ * Code für Übungsblatt 00
+ *
+ * @author Ruben Deisenroth
  */
 public class Main {
 
-    /**
-     * Main entry point in executing the program.
-     *
-     * @param args program arguments, currently ignored
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final int worldSize = 5;
+        World.setSize(worldSize, worldSize);
+        World.setDelay(50);
+        World.setVisible(true);
+        Robot robby = new Robot(4, 0, DOWN, 12);
+        doExercise(robby);
+    }
+
+    /**
+     * Bewegt den Roboter entsprechend den Anforderungen von Übungsblatt 00
+     *
+     * @param robby der Roboter, der bewegt werden soll
+     */
+    public static void doExercise(Robot robby) {
+        // TODO H00 Implement your solution here:
+
+        // Zunächst drehen wir uns nach oben
+        while (robby.getDirection() != UP) {
+            robby.turnLeft();
+        }
+        // Dann laufen wir nach oben und legen nach jedem schritt eine Münze ab
+        for (int i = 0; i < World.getHeight() - 1; i++) {
+            robby.move();
+            robby.putCoin();
+        }
+        // nach links schauen
+        robby.turnLeft();
+
+        // jetzt gehen wir stufenweise nach Unten links
+        for (int i = 0; i < 4; i++) {
+            // Schritt nach Links + Münze legen
+            robby.move();
+            robby.putCoin();
+
+            // Nach unten schauen
+            robby.turnLeft();
+
+            // Schritt nach Unten
+            robby.move();
+            robby.putCoin();
+
+            // Rechtsdrehung
+            robby.turnLeft();
+            robby.turnLeft();
+            robby.turnLeft();
+        }
     }
 }
