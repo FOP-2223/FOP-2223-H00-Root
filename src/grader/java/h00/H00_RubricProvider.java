40a35cd08cd88c3286aaeabe9196e964c64036be
Ruben Deisenroth <rdeisenroth@gmail.com>
2022-06-08 01:03:20 +0200

    Add Tests and Rubric Provider

--- src/grader/java/h00/H00_RubricProvider.java
+++ src/grader/java/h00/H00_RubricProvider.java
@@ -1,18 +1,87 @@
 package h00;
 
-import org.sourcegrade.jagr.api.rubric.Rubric;
-import org.sourcegrade.jagr.api.rubric.RubricForSubmission;
-import org.sourcegrade.jagr.api.rubric.RubricProvider;
+import org.sourcegrade.jagr.api.rubric.*;
 
 @RubricForSubmission("h00")
 public class H00_RubricProvider implements RubricProvider {
 
-    public static final Rubric RUBRIC = Rubric.builder()
-        .title("H00")
-        .build();
-
     @Override
     public Rubric getRubric() {
-        return RUBRIC;
+        return Rubric
+            .builder()
+            .title("H00")
+            .addChildCriteria(
+                Criterion
+                    .builder()
+                    .shortDescription("H4: Erste Schritte mit FOPBot")
+                    .addChildCriteria(
+                        Criterion
+                            .builder()
+                            .shortDescription("Der Roboter erreicht die obere rechte Ecke mindestens einmal")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testUpperRightCornerReached")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die Endposition des Roboters ist immer exakt korrekt.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die Bewegungen des Roboters (Position und Blickrichtung) sind vollständig korrekt.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build(),
+                        Criterion
+                            .builder()
+                            .shortDescription("Die korrekte Anzahl an Münzen wurde an den geforderten Stellen richtig platziert.")
+                            .grader(
+                                Grader
+                                    .testAwareBuilder()
+                                    .requirePass(
+                                        JUnitTestRef.ofMethod(
+                                            () -> TutorTests.class.getDeclaredMethod("testEndPositionCorrect")
+                                        )
+                                    )
+                                    .pointsFailedMin()
+                                    .pointsPassedMax()
+                                    .build()
+                            )
+                            .build()
+                    )
+                    .build()
+            )
+            .build();
     }
 }
